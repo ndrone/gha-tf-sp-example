a4ddfbed51dd8a2a82badd370b651b563aa1429d
Nicholas Drone <ndrone@gmail.com>
2021-11-02 17:28:27 -0400

    Add 2 Service principals

--- main.tf
+++ main.tf
@@ -11,11 +11,18 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "2.83.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.8.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+provider "azuread" {
 }
 
 resource "azurerm_resource_group" "example" {
@@ -47,3 +54,48 @@ resource "azurerm_key_vault" "example" {
     ]
   }
 }
+
+data "azuread_client_config" "current" {}
+
+resource "azuread_application" "example1" {
+  display_name = "example1"
+  owners       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal" "example1" {
+  application_id               = azuread_application.example1.application_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal_password" "example1" {
+  service_principal_id = azuread_service_principal.example1.object_id
+}
+
+resource "azurerm_key_vault_secret" "example1" {
+  name         = "example1"
+  value        = azuread_service_principal_password.example1.value
+  key_vault_id = azurerm_key_vault.example.id
+}
+
+resource "azuread_application" "example2" {
+  display_name = "example2"
+  owners       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal" "example2" {
+  application_id               = azuread_application.example2.application_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal_password" "example2" {
+  service_principal_id = azuread_service_principal.example2.object_id
+}
+
+resource "azurerm_key_vault_secret" "example2" {
+  name         = "example2"
+  value        = azuread_service_principal_password.example2.value
+  key_vault_id = azurerm_key_vault.example.id
+}
+
